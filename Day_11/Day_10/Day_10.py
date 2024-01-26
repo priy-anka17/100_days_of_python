e42c5490acd4baa9d1a43967b8059399ae13573f
priy-anka17 <116798799+priy-anka17@users.noreply.github.com>
2024-01-26 23:01:03 +0530

    Add files via upload

---
+++ Day_11/Day_10/Day_10.py
@@ -0,0 +1,23 @@
+## Function with Output
+
+def format_name(f_name, l_name):
+  formated_f_name = f_name.title()
+  formated_l_name = l_name.title()
+  return (f"{formated_f_name} {formated_l_name}")
+
+
+print(format_name("anshu", "KUMAR"))
+  
+
+
+def format_name(f_name, l_name):
+  if f_name == "" or l_name =="":
+    return "You didn't provide valid inputs."
+  formated_f_name = f_name.title()
+  formated_l_name = l_name.title()
+  return (f"{formated_f_name} {formated_l_name}")
+
+
+print(format_name(input("What is your first name? "), 
+input("What is your last name?")))
+  
