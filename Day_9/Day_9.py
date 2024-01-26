5269fb43af12746f73a90d52cc133163550f31b2
priy-anka17 <116798799+priy-anka17@users.noreply.github.com>
2024-01-26 23:00:17 +0530

    Add files via upload

---
+++ Day_9/Day_9.py
@@ -0,0 +1,58 @@
+##Nesting
+
+capitals = {
+    "France": "Paris",
+    "Germany":"Berlin",
+}
+
+travel_log = {
+    "France": {"cities_visited": ["Parises", "Lille" , "Dijon"], "total_visitis":12},
+    "Germany":["Berlin","Hamburg", "Sruttgart"]
+}
+
+
+## Nesting Dictionary in a list
+
+travel_log = [
+{
+  "country": "France", 
+  "cities_visited": ["Parises", "Lille" , "Dijon"], 
+  "total_visitis":12
+},
+{
+  "country": "Germany", 
+  "cities_visited": ["Berlin","Hamburg", "Sruttgart"], 
+  "total_visits": 5
+},
+]
+
+
+from replit import clear
+from art import logo
+print(logo)
+
+bids = {}
+bidding_finished = False
+
+def find_highest_bidder(bidding_record):
+  highest_bid = 0
+  winner = ""
+  # bidding_record = {"Angela": 123, "James": 321}
+  for bidder in bidding_record:
+    bid_amount = bidding_record[bidder]
+    if bid_amount > highest_bid: 
+      highest_bid = bid_amount
+      winner = bidder
+  print(f"The winner is {winner} with a bid of ${highest_bid}")
+
+while not bidding_finished:
+  name = input("What is your name?: ")
+  price = int(input("What is your bid?: $"))
+  bids[name] = price
+  should_continue = input("Are there any other bidders? Type 'yes or 'no'.\n")
+  if should_continue == "no":
+    bidding_finished = True
+    find_highest_bidder(bids)
+  elif should_continue == "yes":
+    clear()
+  
